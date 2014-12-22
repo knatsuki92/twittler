e012fe97c7260ecd304d80cc40994e131c1d9ce0
Kuni Natsuki <knatsuki92@gmail.com>
2014-12-22 15:48:50 +0900

    Created the template for CreateTweetMsg().

--- javascript/twittlerRender.js
+++ javascript/twittlerRender.js
@@ -1,16 +1,22 @@
     
 $(document).ready(function(){
-  var $TweetBox = $('.TweetBox');
-  //$TweetBox.html('');
+  var $TweetBox = $('.TweetBox'),
+      //This function creates and formats the <div> rendered in html for each tweet.  
+      CreateTweetMsg = function(tweet){
+        var $tweet = $('<div></div>');
+        $tweet.text('@' + tweet.user + ': ' + tweet.message);
+
+        return $tweet;
+
+      };
 
   var index = streams.home.length - 1,
       lastIndex = index;
 
-
   while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    var tweet = streams.home[index],
+        $tweet = CreateTweetMsg(tweet);
+    
     $tweet.appendTo($TweetBox);
     index -= 1;
   }
@@ -25,8 +31,7 @@ $(document).ready(function(){
     while(refreshIndex > prevIndex){
       var tweet = streams.home[refreshIndex];
       console.log(tweet);
-      var $tweet = $('<div></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      var $tweet = CreateTweetMsg(tweet);
       $tweet.prependTo($TweetBox);
       refreshIndex -= 1;
     }
