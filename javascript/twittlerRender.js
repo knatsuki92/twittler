c898a2c954ea6a8974af55464974502f57105cbc
Kuni Natsuki <knatsuki92@gmail.com>
2014-12-23 07:34:11 +0900

    v1 of filterByUser() created.

--- javascript/twittlerRender.js
+++ javascript/twittlerRender.js
@@ -1,9 +1,10 @@
     
 $(document).ready(function(){
-  var $TweetBox = $('.TweetBox'),
+  var $TweetBox = $('.TweetBox').html(""),
+      LastIndex;
 
-      createTweetMsg = function(tweet){
-        //This function creates and formats the <div> rendered in html for each tweet.  
+  var createTweetMsg = function(tweet){
+  //This function creates and formats the <div> rendered in html for each tweet.  
         var $tweet = $('<div class = "tweet"></div>'),
             $user =  $('<div class = "user"></div>'),
             $message = $('<div class = "message"></div>'),
@@ -14,34 +15,53 @@ $(document).ready(function(){
                             + tweet.created_at.getMonth() + '/' + tweet.created_at.getDate() + '/' + tweet.created_at.getFullYear();
 
         //render to html view
-        $user.text('@' + tweet.user);
+        $user.html('<a href = # class = ' + tweet.user + '> @' + tweet.user + '</a>');
         $message.text(tweet.message);
         $createdAt.text('created at ' + dateFormatted);
 
         $tweet.append($user).append($message).append($createdAt);
 
         return $tweet;
-      };
+  };
 
-  //initialization that renders all existing tweets in 'streams' object. 
-  var index = streams.home.length - 1,
-      lastIndex = index;
+  var filterByUser = function(user){
+  //This function filters the tweets by user.
+    $TweetBox.html('<div class = "TweetBox"></div>');
 
-  while(index >= 0){
-    var tweet = streams.home[index],
+    var index = streams.users[user].length - 1;
+
+    while(index >= 0){
+      var tweet = streams.users[user][index],
         $tweet = createTweetMsg(tweet);
+
+      $tweet.appendTo($TweetBox);
+      index -= 1;
+
+    }
+  };
+
+  var DisplayAllTweets = function(){
+  //initialization that renders all existing tweets in 'streams' object. 
+    var index = streams.home.length - 1;
     
-    $tweet.appendTo($TweetBox);
-    index -= 1;
-  }
+    LastIndex = index;
+
+    while(index >= 0){
+      var tweet = streams.home[index],
+          $tweet = createTweetMsg(tweet);
+      
+      $tweet.appendTo($TweetBox);
+      index -= 1;
+    }
+  };
 
 
   var refreshTweets = function(){
     //function that updates the .TweetBox with new tweets. 
-    var prevIndex = lastIndex,
+    var prevIndex = LastIndex,
         refreshIndex = streams.home.length - 1;
 
-    lastIndex = refreshIndex;
+    LastIndex = refreshIndex;
 
     while(refreshIndex > prevIndex){
       var tweet = streams.home[refreshIndex];
@@ -52,6 +72,8 @@ $(document).ready(function(){
     }
   };
 
+  DisplayAllTweets();
   $('#refresh').on('click', refreshTweets);
+
 
 });
