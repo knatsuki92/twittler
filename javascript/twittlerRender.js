6ddea5ea4f30504ad6823982a6c436376733af97
Kuni Natsuki <knatsuki92@gmail.com>
2014-12-22 16:44:21 +0900

    formatted the displayed date in twittlerRender.js.

--- javascript/twittlerRender.js
+++ javascript/twittlerRender.js
@@ -2,18 +2,23 @@
 $(document).ready(function(){
   var $TweetBox = $('.TweetBox'),
 
-      CreateTweetMsg = function(tweet){
+      createTweetMsg = function(tweet){
         //This function creates and formats the <div> rendered in html for each tweet.  
         var $tweet = $('<div class = "tweet"></div>'),
             $user =  $('<div class = "user"></div>'),
             $message = $('<div class = "message"></div>'),
             $createdAt = $('<div class = "createdAt"></div>');
 
+        //format 'createdAt' date to be viewer-friendly
+        var dateFormatted = tweet.created_at.getHours() + ':' + tweet.created_at.getMinutes() + ' ' 
+                            + tweet.created_at.getMonth() + '/' + tweet.created_at.getDate() + '/' + tweet.created_at.getFullYear();
+
+        //render to html view
         $user.text('@' + tweet.user);
         $message.text(tweet.message);
-        $createdAt.text('created at ' + tweet.created_at);
+        $createdAt.text('created at ' + dateFormatted);
+
         $tweet.append($user).append($message).append($createdAt);
-        //$tweet.text('@' + tweet.user + ': ' + tweet.message);
 
         return $tweet;
       };
@@ -24,7 +29,7 @@ $(document).ready(function(){
 
   while(index >= 0){
     var tweet = streams.home[index],
-        $tweet = CreateTweetMsg(tweet);
+        $tweet = createTweetMsg(tweet);
     
     $tweet.appendTo($TweetBox);
     index -= 1;
@@ -41,7 +46,7 @@ $(document).ready(function(){
     while(refreshIndex > prevIndex){
       var tweet = streams.home[refreshIndex];
       console.log(tweet);
-      var $tweet = CreateTweetMsg(tweet);
+      var $tweet = createTweetMsg(tweet);
       $tweet.prependTo($TweetBox);
       refreshIndex -= 1;
     }
